e8bc71fe34f67c37096501f8ad1076318bbef77a
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-06-12 15:55:18 +0300

    Exam 27June

--- ExamPrep/Exam 27 june 2020/Solutions.sql
+++ ExamPrep/Exam 27 june 2020/Solutions.sql
@@ -1,90 +1,3 @@
-CREATE DATABASE WMS
-
---01. Design Database
-CREATE TABLE [Clients]
-(
-	[ClientId] INT PRIMARY KEY IDENTITY,
-    [FirstName] VARCHAR(50) NOT NULL,
-    [LastName] VARCHAR(50) NOT NULL,
-    [Phone] CHAR(12) NOT NULL,
-	CHECK (LEN([PHONE]) = 12) --Min Lenght
-)
-
-CREATE TABLE [Mechanics]
-(
-	[MechanicId] INT PRIMARY KEY IDENTITY,
-    [FirstName] VARCHAR(50) NOT NULL,
-    [LastName] VARCHAR(50) NOT NULL,
-    [Address] VARCHAR(255) NOT NULL,
-)
-
-
-CREATE TABLE [Models]
-(
-	[ModelId] INT PRIMARY KEY IDENTITY,
-    [Name] VARCHAR(50) NOT NULL UNIQUE,
-)
-
-CREATE TABLE [Jobs]
-(
-	[JobId] INT PRIMARY KEY IDENTITY,
-    [ModelId] INT FOREIGN KEY REFERENCES [Models]([ModelId]) NOT NULL,
-    [Status] VARCHAR(11) NOT NULL DEFAULT('Pending'), --Default Value
-    [ClientId] INT FOREIGN KEY REFERENCES [Clients]([ClientId]) NOT NULL,
-    [MechanicId] INT FOREIGN KEY REFERENCES [Mechanics]([MechanicId]),
-    [IssueDate] DATE NOT NULL,
-    [FinishDate] DATE,
-
-	CHECK ([Status] IN ('Pending', 'In Progress', 'Finished')) --Allowed values
-)
-
-CREATE TABLE [Orders]
-(
-	[OrderId] INT PRIMARY KEY IDENTITY,
-    [JobId] INT FOREIGN KEY REFERENCES [Jobs]([JobId]) NOT NULL,
-    [IssueDate] DATE,
-    [Delivered] BIT NOT NULL DEFAULT(0), --FALSE
-)
-
-CREATE TABLE [Vendors]
-(
-	[VendorId] INT PRIMARY KEY IDENTITY,
-    [Name] VARCHAR(50) NOT NULL UNIQUE
-)
-
-CREATE TABLE [Parts]
-(
-	[PartId] INT PRIMARY KEY IDENTITY,
-    [SerialNumber] VARCHAR(50) NOT NULL UNIQUE,
-    [Description] VARCHAR(255),
-    [Price] DECIMAL(6,2) NOT NULL,
-    [VendorId] INT FOREIGN KEY REFERENCES [Vendors]([VendorId]) NOT NULL,
-    [StockQty] INT NOT NULL DEFAULT(0),
-
-	CHECK ([Price] > 0),
-	CHECK ([StockQty] >= 0)
-)
-
-CREATE TABLE [OrderParts]
-(
-	[OrderId] INT FOREIGN KEY REFERENCES [Orders]([OrderId]) NOT NULL,
-	[PartId] INT FOREIGN KEY REFERENCES [Parts]([PartId]) NOT NULL,
-	[Quantity] INT NOT NULL DEFAULT(1),
-
-	PRIMARY KEY([OrderId],[PartId]),
-	CHECK ([Quantity] > 0)
-)
-
-CREATE TABLE [PartsNeeded]
-(
-	[JobId] INT FOREIGN KEY REFERENCES [Jobs]([JobId]) NOT NULL,
-	[PartId] INT FOREIGN KEY REFERENCES [Parts]([PartId]) NOT NULL,
-	[Quantity] INT NOT NULL DEFAULT(1),
-
-	PRIMARY KEY([JobId],[PartId]),
-	CHECK ([Quantity] > 0)
-)
-
 --02. Insert
 INSERT INTO Clients (FirstName, LastName, Phone) VALUES
 ('Teri',     'Ennaco' , '570-889-5187'),
@@ -182,6 +95,46 @@ LEFT JOIN Parts p ON p.PartId = op.OrderId
 WHERE j.Status = 'Finished'
 GROUP BY j.JobId
 ORDER BY Total DESC, j.JobId
+
+--5.6
+SELECT * 
+FROM (SELECT p.PartId,
+			 p.Description,
+			 pn.Quantity AS [Required],
+			 p.StockQty AS [In Stock],
+			 ISNULL(op.Quantity, 0) AS [Ordered]
+		FROM Jobs j
+      LEFT JOIN PartsNeeded pn ON pn.JobId = pn.PartId
+      LEFT JOIN Parts p ON pn.PartId = p.PartId
+      LEFT JOIN Orders o ON o.JobId = j.JobId
+      LEFT JOIN OrderParts op ON op.OrderId = o.OrderId
+      WHERE j.[Status] <> 'Finished' AND (o.Delivered = 0 OR o.Delivered IS NULL)
+    ) AS [SubQuery]
+WHERE [Required] > [In Stock] + [Ordered]
+ORDER BY [PartId]
+
+--7 Function
+CREATE FUNCTION udf_GetCost(@jobId INT)
+RETURNS DECIMAL(8,2)
+AS
+BEGIN
+    DECLARE @result DECIMAL(8,2)
+    DECLARE @jobsOrderCount INT = (SELECT COUNT(OrderId) FROM Jobs j 
+								   LEFT JOIN Orders o ON o.JobId = j.JobId
+								   WHERE j.JobId = @jobId)
+
+	IF @jobsOrderCount = 0
+    BEGIN
+	RETURN 0
+	END
+
+	SET @result = (SELECT SUM(p.Price * op.Quantity) FROM Jobs j 
+				   LEFT JOIN Orders o ON o.JobId = j.JobId
+				   LEFT JOIN OrderParts op ON o.OrderId = op.OrderId
+				   LEFT JOIN Parts p ON p.PartId = op.PartId
+				   WHERE j.JobId = @jobId)
+	RETURN @result
+END
 
 
 
